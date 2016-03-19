8cf269fe4bf57ac6c03a2768b971b51c6869c97d
Randy Hoffner <rnh@ashlandoregon.org>
2016-03-19 16:30:17 -0700

    Added files via upload

---
+++ frontend-nanodegree-arcade-game-randy/README.md
@@ -0,0 +1,8 @@
+frontend-nanodegree-arcade-game
+===============================
+
+#  Description
+This arcade game is a project for the Front End Web Development Nanodegree course.
+
+# How to Play
+Clone or copy the game to your computer.  Open the game by clicking on the index.html file.  The player begins at the bottom of the grass.  You must navigate him across the stones to the water at the top of the canvas using the arrow keys, without colliding with any bugs.  If you collide with a bug, you will see the message "OOPS!  Yer done."  Hit "RETURN" or click "OK" to reset the player and restart the game.  If you reach the water, you will receive the message "Yaaay!  Ya Win!".  Again, Hit "RETURN" or click "OK" to reset the player. 
---
+++ frontend-nanodegree-arcade-game-randy/js/app.js
@@ -0,0 +1,154 @@
+
+var LEN_X = 101;
+var LEN_Y = 83;
+var PLAYER_START_X_POS = 202;
+var PLAYER_START_Y_POS = 420;
+var PLAYER_MIN_X_POS = 0;
+var PLAYER_MIN_Y_POS = -40;
+
+var PLAYER_MAX_X_POS = 4 * LEN_X;
+var PLAYER_MAX_Y_POS = 5 * LEN_Y;
+
+
+var ENEMY_MAX_X_POS = 5 * LEN_X;
+
+
+// Enemies our player must avoid
+var Enemy = function(startX,startY) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.x = startX;
+    this.y = startY;
+    this.height = 50;
+    this.width = 101;
+    this.speed = Math.floor(Math.random() *5 +1) *100;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if (this.x > ENEMY_MAX_X_POS) {
+        this.x = -(Math.floor((Math.random() * 5) + 1) * LEN_X);
+        this.y = Math.floor((Math.random() * 3) + 1) * LEN_Y;
+    }else {
+        this.x = this.x + (this.speed * dt);
+    }
+    var bugXLeftRange = this.x - 50;
+    var bugXRightRange = this.x + 50;
+    var bugYTopRange = this.y -50;
+    var bugYBottomRange = this.y + 50;
+
+//collision detection
+    if (player.x > bugXLeftRange && player.x < bugXRightRange && player.y > bugYTopRange && player.y < bugYBottomRange) {
+
+    player.reset();
+    alert ('OOPS! Yer done!  Hit RETURN to Restart');
+    }
+
+};
+
+
+Enemy.prototype.reset = function() {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(x,y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = PLAYER_START_X_POS;
+    this.y = PLAYER_START_Y_POS;
+    this.height = 50;
+    this.width = 50;
+    };
+
+Player.setSprite = function() {
+    this.sprite = $('.active').attr('src');
+};
+
+//player reaches the water and wins
+Player.update = function(dt) {
+    if (player.y <= 20) {
+        player.reset();
+        alert('Yaaay!  Ya win!');
+    }
+};
+
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+Player.prototype.handleInput = function(direction) {
+    if (direction === 'left' && this.x >25) {
+        this.x -= 100;
+    }
+    if (direction === 'up' && this.y > 0) {
+        this.y -= 61;
+
+        }
+
+
+
+    if (direction === 'right' && this.x < 400) {
+        this.x += 100;
+    }
+    if (direction === 'down' && this.y < 400) {
+        this.y += 82.5;
+    }
+
+};
+
+
+
+Player.prototype.reset = function(){
+    this.x = PLAYER_START_X_POS;
+    this.y = PLAYER_START_Y_POS;
+
+};
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var enemyBottom = new Enemy(-50, 363, 1);
+var enemyMiddle = new Enemy(-50, 242, 2);
+var enemyTop = new Enemy(-50, 121, 3);
+var allEnemies = [];
+allEnemies.push(enemyBottom, enemyMiddle, enemyTop);
+
+
+var player = new Player();
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
